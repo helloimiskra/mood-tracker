a0eaba08f0379ad377430396d50bf6afb42ce63e
helloimiskra <helloimiskra@gmail.com>
2020-10-26 20:33:53 +0900

    adds some folders and files, adds class structure to containers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import './App.css';
 
 
 class App extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/users')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
   render() {
     return (
       <div className="App">
