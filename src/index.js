6b71eb92209ca40f7a4b34fccdebdc4dcabe4643
helloimiskra <helloimiskra@gmail.com>
2020-10-26 20:44:09 +0900

    successfully connects and displays hello world

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import  {BrowserRouter as Router} from 'react-router-dom'
 
 import App from './App';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}> 
