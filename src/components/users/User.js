749b55a7f180b14c994eedb64138e73681d0a494
helloimiskra <helloimiskra@gmail.com>
2020-11-01 02:58:36 +0900

    creates user component

---
+++ src/components/users/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const User = (props) => {
+    return (
+        <div>
+             <li key = {user.id}>
+                    {user.username}
+                </li>)
+            
+        </div>
+    );
+};
+
+export default User;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import User from './User'
 
 export const Users = ({users}) => {
     return (
         <div>
             <h1>Current users</h1>
-            {users.map(user=>
-                <li key = {user.id}>
-                    {user.username}
-                </li>)}
+            {users.map(
+                <User user = {user}/>
+            )}
+                
+               
         </div>
     )
 }
